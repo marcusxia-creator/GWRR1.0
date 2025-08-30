65ef7ba7b42bae72974c9a2d9fd712afb4720c01
gxia <xiaguowei@gmail.com>
2025-08-30 01:23:11 -0600

    <RR1.0 Action - Premier Bot>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FirstRoadRunnerAction.java
@@ -0,0 +1,109 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="First RR1.0 Actions")
+public class FirstRoadRunnerAction extends LinearOpMode {
+
+    /**
+     * Override this method and place your code here.
+     * <p>
+     * Please do not catch {@link InterruptedException}s that are thrown in your OpMode
+     * unless you are doing it to perform some brief cleanup, in which case you must exit
+     * immediately afterward. Once the OpMode has been told to stop, your ability to
+     * control hardware will be limited.
+     *
+     * @throws InterruptedException When the OpMode is stopped while calling a method
+     *                              that can throw {@link InterruptedException}
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap,new Pose2d(0,0,0));
+        Servo servo = hardwareMap.servo.get("servo");
+        Pose2d initialPose = new Pose2d(0,0,0);
+        TrajectoryActionBuilder driveForwards = drive.actionBuilder(initialPose)
+                        .lineToX(0);
+        TrajectoryActionBuilder intakeSamples = driveForwards.endTrajectory().fresh()
+                        .strafeToLinearHeading(new Vector2d(-4,45), Math.toRadians(90))
+                        .lineToX(12)
+                        .stopAndAdd(servo.setPosition(0.3))
+                        .afterDisp(12,servo.setPosition(0.6))
+                        .lineToXConstantHeading(10)
+                        .stopAndAdd(servo.setPosition(0));
+
+        waitForStart();
+
+
+
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(0, 0, 0))
+                        .lineToX(64)
+                        .stopAndAdd(new PatientServoAction(servo,0))
+                        .stopAndAdd(new ServoAction(servo,0.5))
+                        .lineToX(0)
+                        .stopAndAdd(new PatientServoAction(servo,1.0))
+                        .waitSeconds(1)
+                        .build());
+
+    }
+
+    public class ServoAction implements Action {
+        Servo servo;
+        double position;
+
+        public ServoAction(Servo s, double p)
+        {
+            this.servo = s;
+            this.position = p;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            servo.setPosition(position);
+            return false;
+        }
+    }
+
+    public class PatientServoAction implements Action {
+        Servo servo;
+        double position;
+        ElapsedTime timer;
+        boolean hasInitialized = false;
+
+        public PatientServoAction(Servo s, double p)
+        {
+            this.servo = s;
+            this.position = p;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            if (timer == null || !hasInitialized){
+                hasInitialized = true;
+                timer = new ElapsedTime();
+                servo.setPosition(position);
+                robot.intakeSlides.slidesMotor.setPower(0);     // only run once
+            }
+            // do we need to keep running?
+            robot.intakeSlide.slidesMotor.setPower(0.1);
+            return timer.seconds() < 3;
+            //return motor.getPosition() >= targetPosition;
+        }
+
+        public Action cool (Robot robot){
+            return new cool(robot);
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/tuning/TuningOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/tuning/TuningOpModes.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.tuning;
+package org.firstinspires.ftc.teamcode.Auto.tuning;
 
 import androidx.annotation.NonNull;
 
@@ -40,12 +40,12 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.OTOSLocalizer;
-import org.firstinspires.ftc.teamcode.PinpointLocalizer;
-import org.firstinspires.ftc.teamcode.TankDrive;
-import org.firstinspires.ftc.teamcode.ThreeDeadWheelLocalizer;
-import org.firstinspires.ftc.teamcode.TwoDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.Auto.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Auto.OTOSLocalizer;
+import org.firstinspires.ftc.teamcode.Auto.PinpointLocalizer;
+import org.firstinspires.ftc.teamcode.Auto.TankDrive;
+import org.firstinspires.ftc.teamcode.Auto.ThreeDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.Auto.TwoDeadWheelLocalizer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
